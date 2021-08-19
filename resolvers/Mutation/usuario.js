e06c9fbea1fef90863bcb3e9524f01010783895d
Alexandre Florentino <kanemaki@gmail.com>
2021-08-19 23:47:16 +0100

    feat: add bcrypt backend env Graphql.

--- resolvers/Mutation/usuario.js
+++ resolvers/Mutation/usuario.js
@@ -1,20 +1,30 @@
+const bcrypt = require('bcrypt-nodejs')
 const db = require('../../config/db')
 const { perfil: obterPerfil } = require('../Query/perfil')
 const { usuario: obterUsuario } = require('../Query/usuario')
 
-module.exports = {
+const mutations = {
     async novoUsuario(_, { dados }) {
         try {
             const idsPerfis = []
-            if(dados.perfis) {
-                for(let filtro of dados.perfis) {
-                    const perfil = await obterPerfil(_, {
-                        filtro
-                    })
-                    if(perfil) idsPerfis.push(perfil.id)
-                }
+              
+            if(!dados.perfis || !dados.perfis.length) {
+                dados.perfis =[{
+                    nome: 'comum'
+                }]
+            }
+         
+            for(let filtro of dados.perfis) {
+                const perfil = await obterPerfil(_, {
+                    filtro
+                })
+                if(perfil) idsPerfis.push(perfil.id)
             }
 
+            // criptografar a senha
+            const salt = bcrypt.genSaltSync()
+            dados.senha = bcrypt.hashSync(dados.senha, salt)
+          
             delete dados.perfis
             const [ id ] = await db('usuarios')
                 .insert(dados)
@@ -70,6 +80,12 @@ module.exports = {
                     }
                 }
 
+                if(dados.senha){
+                    // criptografar a senha
+                    const salt = bcrypt.genSaltSync()
+                    dados.senha = bcrypt.hashSync(dados.senha, salt)
+                }
+
                 delete dados.perfis
                 await db('usuarios')
                     .where({ id })
@@ -81,3 +97,5 @@ module.exports = {
         }
     }
 }
+
+module.exports = mutations
