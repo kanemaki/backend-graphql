4e515577aa6ef70e342214473f84c5c0d2453aa2
Alexandre Florentino <kanemaki@gmail.com>
2021-08-23 21:06:23 +0100

    feat: add new context.

--- config/context.js
+++ config/context.js
@@ -1,9 +1,3 @@
 module.exports = ({ req }) => {
-    console.log('Contexto')
-    return {
-        texto: 'Curso GraphQL',
-        imprimir() {
-            console.log('Curso GraphQL!!!!!')
-        }
-    }
+    const auth = req.headers.authorization
 }
