7af10dc06f00983822e18a48a90e10f9a8e136b2
Alexandre Florentino <kanemaki@gmail.com>
2021-08-24 23:18:07 +0100

    feat: add mock usuariologado.

--- config/context.js
+++ config/context.js
@@ -1,8 +1,41 @@
-module.exports = async ({ req }) => {
-    
+const jwt = require('jwt-simple')
+
+module.exports = async ({ req }) => { 
     // Em desenvolvimento
     await require('./simularUsuarioLogado')(req)
 
     const auth = req.headers.authorization
-    console.log(auth)
+    const token = auth && auth.sustring(7)
+
+    let usuario = null
+    let admin = false
+
+    if(token) {
+        try {
+            let conteudoToken = jwt.decode(token, process.env.APP_AUTH_SECRET)
+            
+            if(new Date(conteudoToken.exp * 1000) > new Date()) {
+                usuario = conteudoToken                        
+            }
+        } catch(e) {
+            // token invalido
+        }
+    }
+
+    if(usuario && usuario.perfis) {
+        admin = usuario.perfis.includes('admin')
+    }
+
+    const err = new Error('Acesso Negado!')
+
+    return {
+        usuario,
+        admin,
+        validarUsuario() {
+            if(!usuario) throw err
+        },
+        validarAdmin() {
+            if(!admin) throw err
+        }
+    }
 }
